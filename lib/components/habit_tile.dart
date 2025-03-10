6235ab17e543136a51dee385e33579f4b093fdc7
juuann7 <muhamadnajwan20@gmail.com>
2025-03-10 11:01:56 +0700

    first commit

---
+++ lib/components/habit_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class HabitTile extends StatelessWidget {
+  final String habitName;
+  final bool habitCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? settingsTapped;
+  final Function(BuildContext)? deleteTapped;
+
+  const HabitTile({
+    super.key,
+    required this.habitName,
+    required this.habitCompleted,
+    required this.onChanged,
+    required this.settingsTapped,
+    required this.deleteTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            //settings
+            SlidableAction(
+              onPressed: (context) => settingsTapped!(context),
+              backgroundColor: Colors.grey.shade800,
+              icon: Icons.settings,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            //delete
+            SlidableAction(
+              onPressed: (context) => deleteTapped!(context),
+              backgroundColor: Colors.red.shade400,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.grey[600],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              // checkbox
+              Checkbox(value: habitCompleted, onChanged: onChanged),
+
+              //habit name
+              Text(habitName),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,134 @@
+import "package:flutter/material.dart";
+import "package:habit_tracker/components/habit_tile.dart";
+import "package:habit_tracker/components/month_summary.dart";
+import "package:habit_tracker/components/my_fab.dart";
+import "package:habit_tracker/components/my_alert_box.dart";
+import "package:habit_tracker/data/habit_database.dart";
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  HabitDatabase db = HabitDatabase();
+  final _myBox = Hive.box("Habit_Database");
+
+  @override
+  void initState() {
+    if (_myBox.get("CURRENT_HABIT_LIST") == null) {
+      db.createDefaultData();
+    } else {
+      db.loadData();
+    }
+    db.updateDatabase();
+
+    super.initState();
+  }
+
+  // checkbox was tapped
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      db.todaysHabitList[index][1] = value;
+    });
+    db.updateDatabase();
+  }
+
+  final _newHabitNameController = TextEditingController();
+  void createNewHabit() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return MyAlertBox(
+            controller: _newHabitNameController,
+            onSave: saveNewHabit,
+            onCancel: cancelDialogBox,
+            hintText: "Enter Habit Name..",
+          );
+        });
+  }
+
+  void saveNewHabit() {
+    setState(() {
+      db.todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+    _newHabitNameController.clear();
+    Navigator.of(context).pop();
+    db.updateDatabase();
+  }
+
+  void cancelDialogBox() {
+    setState(() {
+      db.todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+    _newHabitNameController.clear();
+    Navigator.of(context).pop();
+  }
+
+  // open habit settings
+  void openHabitSettings(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          onSave: () => saveExistingHabit(index),
+          onCancel: cancelDialogBox,
+          hintText: db.todaysHabitList[index][0],
+        );
+      },
+    );
+  }
+
+  void saveExistingHabit(int index) {
+    setState(() {
+      db.todaysHabitList[index][0] = _newHabitNameController.text;
+    });
+    _newHabitNameController.clear();
+    Navigator.pop(context);
+    db.updateDatabase();
+  }
+
+  void deleteHabit(int index) {
+    setState(() {
+      db.todaysHabitList.removeAt(index);
+    });
+    db.updateDatabase();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[500],
+        floatingActionButton: MyFloatingActionButton(
+          onPressed: createNewHabit,
+        ),
+        body: ListView(
+          children: [
+            //monthly summary heatmap
+            MonthlySummary(
+                datasets: db.heatMapDataSet,
+                startDate: _myBox.get("START_DATE")),
+
+            //list of habits
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: db.todaysHabitList.length,
+              itemBuilder: (context, index) {
+                return HabitTile(
+                  habitName: db.todaysHabitList[index][0],
+                  habitCompleted: db.todaysHabitList[index][1],
+                  onChanged: (value) => checkBoxTapped(value, index),
+                  settingsTapped: (context) => openHabitSettings(index),
+                  deleteTapped: (context) => deleteHabit(index),
+                );
+              },
+            ),
+          ],
+        ));
+  }
+}
